14a3af07cc238543acf8b879479a2c02fb651882
CornerstoneII <seunmyles@gmail.com>
2020-12-08 18:10:33 +0100

    Implement the user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :username, :email, :password, presence: true
+    validates :username, length: {minimum: 4}
+    validates :password, length: {minimum: 6}
 end
